96a7431c2589afebf0b3885b2db1daaa516e440a
André Rosatto <linus1178@gmail.com>
2024-07-03 09:35:18 -0300

    v1

--- server.js
+++ server.js
@@ -6,11 +6,5 @@ app.use(express.static(__dirname + '/src'));
 app.get('/', (_, res) => {
     res.sendFile((__dirname + '/src/index.html'));
 });
-app.get('/videos', (_, res) => {
-    res.sendFile((__dirname + '/src/index.html'));
-});
-app.get('/favoritos', (_, res) => {
-    res.sendFile((__dirname + '/src/favorites.html'));
-});
 app.listen(PORT);
 console.log(`Serving at localhost:${PORT}`);
--- server.ts
+++ server.ts
@@ -9,13 +9,5 @@ app.get('/', (_: any, res: any) => {
 	res.sendFile((__dirname + '/src/index.html'));
 });
 
-app.get('/videos', (_: any, res: any) => {
-	res.sendFile((__dirname + '/src/index.html'));
-});
-
-app.get('/favoritos', (_: any, res: any) => {
-	res.sendFile((__dirname + '/src/favorites.html'));
-});
-
 app.listen(PORT);
 console.log(`Serving at localhost:${PORT}`);
